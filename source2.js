21661e80b8b02ea7945ab7117c2f5b1e81269216
Rahul Deshmukh <rahul.a.deshmukh@gmail.com>
2017-10-02 12:04:26 -0400

    1. Removed Video embed. 2. Add medium.txt. 3. Updated source2.js to download medium.txt

--- source2.js
+++ source2.js
@@ -2,7 +2,7 @@ $(function(){
     $("#startIFrameCall").click(
       function(){
           $("#status").text("Starting AJAX download...");
-          $.ajax({url: "http://www.pomeeno.com/download/large.txt", success: function(result){
+          $.ajax({url: "http://www.pomeeno.com/download/medium.txt", success: function(result){
               $("#status").text("Completed AJAX download");
           }});
       });
